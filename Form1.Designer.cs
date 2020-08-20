5245b03a1486fa0ef8ac9436c074dd43854cbfe8
Макогин Александр <68378330+AleksandrMakogin@users.noreply.github.com>
2020-08-20 17:29:16 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace ДЗ_6_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.newToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.ColorBeacktoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveStripButton = new System.Windows.Forms.ToolStripButton();
+            this.openFiletoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.CopytoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.cuttoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.CancelAllltoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SelectAlltoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripButton,
+            this.openFiletoolStripButton,
+            this.saveStripButton,
+            this.CopytoolStripButton,
+            this.toolStripButton2,
+            this.cuttoolStripButton,
+            this.SelectAlltoolStripButton,
+            this.CancelAllltoolStripButton,
+            this.toolStripButton3,
+            this.ColorBeacktoolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1258, 38);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // newToolStripButton
+            // 
+            this.newToolStripButton.Font = new System.Drawing.Font("Segoe MDL2 Assets", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newToolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.newToolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.newToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolStripButton.Name = "newToolStripButton";
+            this.newToolStripButton.Size = new System.Drawing.Size(41, 35);
+            this.newToolStripButton.Text = "New ";
+            this.newToolStripButton.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.newToolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.newToolStripButton.Click += new System.EventHandler(this.newToolStripButton_Click);
+            // 
+            // ColorBeacktoolStripButton
+            // 
+            this.ColorBeacktoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ColorBeacktoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.ColorBeacktoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.ColorBeacktoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ColorBeacktoolStripButton.Name = "ColorBeacktoolStripButton";
+            this.ColorBeacktoolStripButton.Size = new System.Drawing.Size(74, 35);
+            this.ColorBeacktoolStripButton.Text = "ColorBeack";
+            this.ColorBeacktoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ColorBeacktoolStripButton.Click += new System.EventHandler(this.ColorBeacktoolStripButton_Click);
+            // 
+            // saveStripButton
+            // 
+            this.saveStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.saveStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.saveStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveStripButton.Name = "saveStripButton";
+            this.saveStripButton.Size = new System.Drawing.Size(38, 35);
+            this.saveStripButton.Text = "Save";
+            this.saveStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.saveStripButton.Click += new System.EventHandler(this.saveStripButton_Click);
+            // 
+            // openFiletoolStripButton
+            // 
+            this.openFiletoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFiletoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.openFiletoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.openFiletoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openFiletoolStripButton.Name = "openFiletoolStripButton";
+            this.openFiletoolStripButton.Size = new System.Drawing.Size(41, 35);
+            this.openFiletoolStripButton.Text = "Open";
+            this.openFiletoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.openFiletoolStripButton.Click += new System.EventHandler(this.openFiletoolStripButton_Click);
+            // 
+            // CopytoolStripButton
+            // 
+            this.CopytoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CopytoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.CopytoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.CopytoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CopytoolStripButton.Name = "CopytoolStripButton";
+            this.CopytoolStripButton.Size = new System.Drawing.Size(38, 35);
+            this.CopytoolStripButton.Text = "Copy";
+            this.CopytoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.CopytoolStripButton.Click += new System.EventHandler(this.CopytoolStripButton_Click);
+            // 
+            // cuttoolStripButton
+            // 
+            this.cuttoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cuttoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.cuttoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.cuttoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cuttoolStripButton.Name = "cuttoolStripButton";
+            this.cuttoolStripButton.Size = new System.Drawing.Size(30, 35);
+            this.cuttoolStripButton.Text = "Cut";
+            this.cuttoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.cuttoolStripButton.Click += new System.EventHandler(this.cuttoolStripButton_Click);
+            // 
+            // CancelAllltoolStripButton
+            // 
+            this.CancelAllltoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CancelAllltoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.CancelAllltoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.CancelAllltoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CancelAllltoolStripButton.Name = "CancelAllltoolStripButton";
+            this.CancelAllltoolStripButton.Size = new System.Drawing.Size(47, 35);
+            this.CancelAllltoolStripButton.Text = "Cancel";
+            this.CancelAllltoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.CancelAllltoolStripButton.Click += new System.EventHandler(this.CanceltoolStripButton_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripButton2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.toolStripButton2.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(44, 35);
+            this.toolStripButton2.Text = "Insert";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripButton3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.toolStripButton3.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(65, 35);
+            this.toolStripButton3.Text = "ColorFont";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(423, 248);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 227);
+            this.textBox1.TabIndex = 1;
+            // 
+            // SelectAlltoolStripButton
+            // 
+            this.SelectAlltoolStripButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectAlltoolStripButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.SelectAlltoolStripButton.Image = global::ДЗ_6_1_.Properties.Resources.Generic_36503;
+            this.SelectAlltoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SelectAlltoolStripButton.Name = "SelectAlltoolStripButton";
+            this.SelectAlltoolStripButton.Size = new System.Drawing.Size(60, 35);
+            this.SelectAlltoolStripButton.Text = "SelectAll";
+            this.SelectAlltoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.SelectAlltoolStripButton.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = global::ДЗ_6_1_.Properties.Resources.Шаг3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1258, 569);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.ForeColor = System.Drawing.Color.Peru;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Макогин Александр  ДЗ6_1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton newToolStripButton;
+        private System.Windows.Forms.ToolStripButton ColorBeacktoolStripButton;
+        private System.Windows.Forms.ToolStripButton saveStripButton;
+        private System.Windows.Forms.ToolStripButton openFiletoolStripButton;
+        private System.Windows.Forms.ToolStripButton CopytoolStripButton;
+        private System.Windows.Forms.ToolStripButton cuttoolStripButton;
+        private System.Windows.Forms.ToolStripButton CancelAllltoolStripButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripButton SelectAlltoolStripButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЗ_6_1_
+{
+    public partial class Form1 : Form
+    {
+        public string path { set; get; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "All Filter(*.*)|*.*|Text Filter(*.txt)|*.txt";
+            dialog.FilterIndex = 2;
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(dialog.FileName);
+                writer.Write(textBox1.Text);
+                writer.Close();
+            }
+        }
+
+        private void openFiletoolStripButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "All Filter(*.*)|*.*|Text Filter(*.txt)|*.txt";
+            open.FilterIndex = 2;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(open.FileName);
+                textBox1.Text = read.ReadToEnd();
+                read.Close();
+
+            }
+        }
+
+        private void saveStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Введите имя файла","Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "All Filter(*.*)|*.*|Text Filter(*.txt)|*.txt";
+            dialog.FilterIndex = 2;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+               
+                StreamWriter writer = new StreamWriter(dialog.FileName);
+                writer.Write(textBox1.Text);
+                writer.Close();
+            }
+        }
+
+        private void CopytoolStripButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+        private void cuttoolStripButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Cut();
+        }
+
+        private void CanceltoolStripButton_Click(object sender, EventArgs e)
+        {
+            if(textBox1.CanUndo == true)
+            {
+                textBox1.Undo();
+                textBox1.ClearUndo();
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.ForeColor = dialog.Color;
+            }
+
+        }
+
+        private void ColorBeacktoolStripButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.BackColor = dialog.Color;
+            }
+        }
+    }
+}
